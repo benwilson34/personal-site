da1bfe749059e7ea625d406cb17ac4c75b9aabf8
Ben Wilson <ben.wilson1337@gmail.com>
2018-11-13 01:43:03 -0500

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ben Wilson';
+  subtitle = "Software Developer";
+  typedSubtitle = "";
+  typeIndex = 0;
+
+  ngOnInit() {
+  	this.typeOutSubtitle();
+  }
+
+  typeOutSubtitle() {
+  	console.log("starting...");
+  	const interval = 100; // 10 chars/sec
+  	var typeAnim = setInterval(() => {
+  		if (this.typeIndex < this.subtitle.length)
+  			this.typeIndex++;
+  		else 
+  			clearInterval(typeAnim);
+  		this.typedSubtitle = this.subtitle.substring(0, this.typeIndex);
+  	}, interval);
+  }
 }
